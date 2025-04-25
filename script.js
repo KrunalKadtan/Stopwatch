61d6ee8e3007eefa70b00aa8e57ae9fd29126040
Krunal Kadtan <krunalkadtan19@gmail.com>
2025-04-25 11:06:57 +0530

    Added Date Input & Improve Look of Displayed Time

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
 
 function calculateTime() {
     const timeDisplay = document.querySelector('#time')
+    const daysDisplay = document.querySelector('#days')
     const inputTime = document.querySelector('#inp-time').value;
-    // const inputDate = document.querySelector('#inp-date').value;
+    const inputDate = document.querySelector('#inp-date').value;
 
-    let inpHours = parseInt(inputTime.split(':')[0]);
-    let inpMinutes = parseInt(inputTime.split(':')[1]);
+    console.log(inputTime, inputDate)
+
+    const [ hours, minutes ] = inputTime.split(':')
+    const [ year, month, day ] = inputDate.split('-')
+    const dateTime = new Date(year, month-1, day, hours, minutes)
 
     let updateTime = setInterval(function() {
 
         let now = new Date();
+        let diff = now - dateTime;
 
-        // let days = now.getDate() - 1;
-        let hours = now.getHours() - inpHours;
-        let minutes = now.getMinutes() - inpMinutes;
-        let seconds = now.getSeconds() - 0;
-        let mseconds = now.getMilliseconds() - 0;
+        let days = Math.floor(diff / (1000 * 60 * 60 * 24));
+        days = (days < 10) ? `0${days}` : days
 
-        // timeDisplay.textContent = `${hours} : ${minutes} : ${seconds}`;
+        let hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        hours = (hours < 10) ? `0${hours}` : hours
+
+        let minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+        minutes = (minutes < 10) ? `0${minutes}` : minutes
+
+        let seconds = Math.floor((diff % (1000 * 60)) / 1000);
+        seconds = (seconds < 10) ? `0${seconds}` : seconds
+
+        let mseconds = Math.floor((diff % (1000)) / 1);
+        mseconds = (mseconds < 100) ? ((mseconds < 10) ? `00${mseconds}` : `0${mseconds}`) : mseconds
+
+        daysDisplay.textContent = `${days}`;
         timeDisplay.textContent = `${hours} : ${minutes} : ${seconds} : ${mseconds}`;
     })
 }
